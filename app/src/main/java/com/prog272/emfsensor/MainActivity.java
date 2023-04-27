3c6f75419876adc66c0bc299da61fd9db32f80aa
gangasrotogati <glass_joseph@hotmail.com>
2023-04-26 21:20:33 -0700

    v0.01

---
+++ app/src/main/java/com/prog272/emfsensor/MainActivity.java
@@ -0,0 +1,68 @@
+package com.prog272.emfsensor;
+
+import android.app.Activity;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends Activity implements SensorEventListener {
+
+    private SensorManager sensorManager;
+    private Sensor magneticFieldSensor;
+    private TextView xValueTextView, yValueTextView, zValueTextView, mValueTextView;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        xValueTextView = findViewById(R.id.xValueTextView);
+        yValueTextView = findViewById(R.id.yValueTextView);
+        zValueTextView = findViewById(R.id.zValueTextView);
+        mValueTextView = findViewById(R.id.mValueTextView);
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        magneticFieldSensor = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+
+        if (magneticFieldSensor == null) {
+            // No magnetic field sensor available on this device
+            finish();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, magneticFieldSensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) {
+            float x = event.values[0];
+            float y = event.values[1];
+            float z = event.values[2];
+            double m = Math.sqrt((x*x) + (y*y) + (z*z));
+
+            xValueTextView.setText("X: " + String.format("%.2f", x) + " μT");
+            yValueTextView.setText("Y: " + String.format("%.2f", y) + " μT");
+            zValueTextView.setText("Z: " + String.format("%.2f", z) + " μT");
+            mValueTextView.setText("M: " + String.format("%.2f", m) + " μT");
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        // Do nothing
+    }
+}
