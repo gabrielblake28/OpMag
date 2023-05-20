e163a41a758804c81c41fbc240f5f945598f7db6
Edward Matsyshen <edward.matsyshen@bellevuecollege.edu>
2023-05-19 18:25:27 -0700

    added firebaseAPI. Connects to firebase, and outputs data per millisecond

--- app/src/main/java/com/prog272/emfsensor/MainActivity.java
+++ app/src/main/java/com/prog272/emfsensor/MainActivity.java
@@ -8,24 +8,53 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.core.view.GestureDetectorCompat;
+
+
+import android.Manifest;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.Timestamp;
+import com.google.firebase.firestore.FieldValue;
+
+
+import java.lang.reflect.Field;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends Activity implements SensorEventListener {
 
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
     private SensorManager sensorManager;
     private Sensor magneticFieldSensor;
     private TextView xValueTextView, yValueTextView, zValueTextView, mValueTextView;
     private GestureDetectorCompat gestureDetectorCompat;
 
+    private static final String TAG = "MainActivity";
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
         xValueTextView = findViewById(R.id.xValueTextView);
         yValueTextView = findViewById(R.id.yValueTextView);
@@ -53,7 +82,7 @@ public class MainActivity extends Activity implements SensorEventListener {
     protected void onResume() {
         super.onResume();
         sensorManager.registerListener(this, magneticFieldSensor, SensorManager.SENSOR_DELAY_NORMAL);
-    }
+    };
 
     @Override
     protected void onPause() {
@@ -67,48 +96,98 @@ public class MainActivity extends Activity implements SensorEventListener {
             float x = event.values[0];
             float y = event.values[1];
             float z = event.values[2];
-            double m = Math.sqrt((x*x) + (y*y) + (z*z));
+            double m = Math.sqrt((x * x) + (y * y) + (z * z));
 
             xValueTextView.setText("X: " + String.format("%.2f", x) + " μT");
             yValueTextView.setText("Y: " + String.format("%.2f", y) + " μT");
             zValueTextView.setText("Z: " + String.format("%.2f", z) + " μT");
             mValueTextView.setText("M: " + String.format("%.2f", m) + " μT");
 
-            if(m >= 100){
+            if (m >= 100) {
                 mValueTextView.setTextColor(getResources().getColor(R.color.level11));
             }
-            if(m < 100){
+            if (m < 100) {
                 mValueTextView.setTextColor(getResources().getColor(R.color.level10));
             }
-            if(m < 90){
+            if (m < 90) {
                 mValueTextView.setTextColor(getResources().getColor(R.color.level9));
             }
-            if(m < 85){
+            if (m < 85) {
                 mValueTextView.setTextColor(getResources().getColor(R.color.level8));
             }
-            if(m < 80){
+            if (m < 80) {
                 mValueTextView.setTextColor(getResources().getColor(R.color.level7));
             }
-            if(m < 75){
+            if (m < 75) {
                 mValueTextView.setTextColor(getResources().getColor(R.color.level6));
             }
-            if(m < 70){
+            if (m < 70) {
                 mValueTextView.setTextColor(getResources().getColor(R.color.level5));
             }
-            if(m < 65){
+            if (m < 65) {
                 mValueTextView.setTextColor(getResources().getColor(R.color.level4));
             }
-            if(m < 60){
+            if (m < 60) {
                 mValueTextView.setTextColor(getResources().getColor(R.color.level3));
             }
-            if(m < 55){
+            if (m < 55) {
                 mValueTextView.setTextColor(getResources().getColor(R.color.level2));
             }
-            if(m < 50){
+            if (m < 50) {
                 mValueTextView.setTextColor(getResources().getColor(R.color.level1));
+            }
+
+
+            if (event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) {
+
+                // PUT new data
+                Map<String, Object> user = new HashMap<>();
+
+                user.put("TimeStamp", FieldValue.serverTimestamp());
+
+                user.put("Microteslas", m);
+
+                user.put("PhoneCoordsX", x);
+                user.put("PhoneCoordsY", y);
+                user.put("PhoneCoordsZ", z);
+
+
+                // Add a new document with a generated ID
+                db.collection("Class Meeting")
+                        .add(user)
+                        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                            @Override
+                            public void onSuccess(DocumentReference documentReference) {
+                                Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Log.w(TAG, "Error adding document", e);
+                            }
+                        });
+
+
+                // Add the new code snippet here
+                db.collection("Class Meetingt")
+                        .get()
+                        .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                if (task.isSuccessful()) {
+                                    for (QueryDocumentSnapshot document : task.getResult()) {
+                                        Log.d(TAG, document.getId() + " => " + document.getData());
+                                    }
+                                } else {
+                                    Log.w(TAG, "Error getting documents.", task.getException());
+                                }
+                            }
+                        });
             }
         }
     }
+
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
